6215c2645b25147d4e72bee541d154cb74d44ffd
mdgg2012 <91275095+mdgg2012@users.noreply.github.com>
2021-10-22 16:48:31 -0300

    Primera_Persona
    Movimiento y posicionamiento de cámara.

---
+++ Assets/Scripts/PrimeraPersona.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimeraPersona : MonoBehaviour
+{
+    private new Transform camera;
+    public Vector2 sensibility;
+
+    void Start()
+    {
+        camera = transform.Find("Camera");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+
+        if (hor != 0)
+        {
+            transform.Rotate(Vector3.up * hor * sensibility);
+        }
+
+        if (ver != 0)
+        {
+            float angle = camera.localEulerAngles.x - ver * sensibility.y;
+            camera.localEulerAngles = Vector3.right * angle;
+        }
+    }
+}
