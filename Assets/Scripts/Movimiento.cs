e6289e080e7123898814a908691b88153a1f9cd2
mdgg2012 <91275095+mdgg2012@users.noreply.github.com>
2021-10-22 17:55:09 -0300

    Movimiento

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float velocidadMovimiento = 5.0f;
+    public float x, y;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+
+    }
+}
