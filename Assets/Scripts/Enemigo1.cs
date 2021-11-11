1f9ff219bef44b3686483a33a48a13036a415a1b
mdgg2012 <91275095+mdgg2012@users.noreply.github.com>
2021-11-11 16:54:10 -0300

    tratando de hacer vida y daño

--- Assets/Scripts/Enemigo1.cs
+++ Assets/Scripts/Enemigo1.cs
@@ -10,21 +10,27 @@ public class Enemigo1 : MonoBehaviour
     public Quaternion angulo;
     public float grado;
 
+    public GameObject target;
+
     void Start()
     {
         ani = GetComponent<Animator>();
+        target = GameObject.Find("Player");
     }
 
     public void Comportamiento_Enemigo()
     {
-        cronometro += 1 * Time.deltaTime;
-        if (cronometro >= 4)
+        if (Vector3.Distance(transform.position, target.transform.position) > 5) 
         {
+            ani.SetBool("run", false);
+          cronometro += 5 * Time.deltaTime;
+          if (cronometro >= 3)
+          {
             rutina = Random.Range(0, 2);
             cronometro = 0;
-        }
-        switch (rutina)
-        {
+          }
+          switch (rutina)
+          {
             case 0:
                 ani.SetBool("walk", false);
                 break;
@@ -35,9 +41,21 @@ public class Enemigo1 : MonoBehaviour
                 break;
             case 2:
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                transform.Translate(Vector3.forward * 6 * Time.deltaTime);
                 ani.SetBool("Walk", true);
                 break;
+          }
+        }
+        else
+        {
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
+            ani.SetBool("walk", false);
+
+            ani.SetBool("run", true);
+            transform.Translate(Vector3.forward * 6 * Time.deltaTime);
         }
     }
 
