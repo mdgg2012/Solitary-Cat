a04d5e699d69154798d86e94dc895c24201f0cb2
mdgg2012 <91275095+mdgg2012@users.noreply.github.com>
2021-11-11 22:29:29 -0300

    teleporte y transportes

---
+++ Assets/Scripts/Teletransporte.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teletransporte : MonoBehaviour
+{
+    public Transform Target;
+    public GameObject ThePlayer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ThePlayer.transform.position = Target.transform.position;
+    }
+}
---
+++ Assets/Scripts/Transporte.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transporte : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed = 6.0f;
+
+    int curPos = 0;
+    int nextPos = 1;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target[nextPos].position, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target[nextPos].position) <= 0)
+        {
+            curPos = nextPos;
+            nextPos++;
+
+            if (nextPos > target.Length - 1)
+                nextPos = 0;
+        }
+    }
+}
